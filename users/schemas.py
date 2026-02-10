fb60f19077062e4ecff914aad5024c1aa71ffcaf
Martin Moreira <172557112+mimoreirac@users.noreply.github.com>
2026-02-10 00:25:03 -0500

    Fix roles

--- users/schemas.py
+++ users/schemas.py
@@ -1,18 +1,21 @@
 import uuid
+from typing import Literal
 from fastapi_users import schemas
 from pydantic import ConfigDict
 
 
 class UserRead(schemas.BaseUser[uuid.UUID]):
     full_name: str
-    role: str
+    role: Literal["client", "tutor", "admin"]
     
     model_config = ConfigDict(from_attributes=True)
 
 
 class UserCreate(schemas.BaseUserCreate):
     full_name: str
+    role: Literal["client", "tutor"] = "client"
 
 
 class UserUpdate(schemas.BaseUserUpdate):
     full_name: str | None = None
+    role: Literal["client", "tutor", "admin"] | None = None
